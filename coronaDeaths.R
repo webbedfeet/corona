07454e3465072c1e4f904dbdad994a8dc219ab79
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-03-24 00:50:10 -0400

    Updated to use new JHU global deaths file

--- coronaDeaths.R
+++ coronaDeaths.R
@@ -3,31 +3,40 @@ library(coronavirus)
 update_datasets(silence=TRUE)
 pacman::p_load(char=c('tidyverse','janitor', 'ggrepel'))
 
-deaths <- coronavirus %>%
+deaths <- read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv') %>%
+  gather(date, deaths, -(1:4)) %>%
   clean_names() %>%
-  filter(type=='death') %>%
+  mutate(date = as.Date(date, format = '%m/%d/%y')) %>%
   group_by(country_region, date) %>%
-  summarise(cases = sum(cases)) %>%
+  summarize(deaths = sum(deaths, na.rm=T)) %>%
   ungroup() %>%
-  group_by(country_region) %>%
-  arrange(date) %>%
-  mutate(cum_cases = cumsum(cases)) %>%
-  ungroup() %>%
-  as_tibble()
+  arrange(date)
+
+# deaths <- coronavirus %>%
+#   clean_names() %>%
+#   filter(type=='death') %>%
+#   group_by(country_region, date) %>%
+#   summarise(cases = sum(cases)) %>%
+#   ungroup() %>%
+#   group_by(country_region) %>%
+#   arrange(date) %>%
+#   mutate(cum_cases = cumsum(cases)) %>%
+#   ungroup() %>%
+#   as_tibble()
 
 top_ten <- deaths %>%
   filter(date==max(date)) %>%
-  top_n(10, cum_cases)
+  top_n(10, deaths)
 
 deaths_top10 <-
   deaths %>%
   filter(country_region %in% top_ten$country_region) %>%
   group_by(country_region) %>%
-  filter(cum_cases >= 10) %>%
+  filter(deaths >= 10) %>%
   mutate(days = as.numeric(date - min(date))) %>%
   ungroup()
 
-(staticplot <- ggplot(deaths_top10, aes(x = days, y = cum_cases)) +
+(staticplot <- ggplot(deaths_top10, aes(x = days, y = deaths)) +
   geom_label_repel(data = deaths_top10 %>%
                      filter(country_region %in% top_ten$country_region) %>%
                      group_by(country_region) %>%
@@ -41,7 +50,7 @@ deaths_top10 <-
                group_by(country_region) %>%
                filter(days == max(days)) %>%
                ungroup(),
-             aes(color = country_region, size=cum_cases),
+             aes(color = country_region, size=deaths),
               show.legend=F)+
     geom_abline(intercept=1, slope = log10(1.33), linetype=2)+
     annotate('text',label = '33% growth', x = 21, y = 10^(1+log10(1.33)*21),
@@ -52,4 +61,4 @@ deaths_top10 <-
   theme_classic()
 )
 
-interactive_plot <- plotly::ggplotly(staticplot)
+# interactive_plot <- plotly::ggplotly(staticplot)
