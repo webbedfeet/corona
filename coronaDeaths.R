6abf47cb4d408ee6110d6ff56381ef229d9d6c2a
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-03-25 02:19:09 -0400

    Updated to use gghighlight

--- coronaDeaths.R
+++ coronaDeaths.R
@@ -1,7 +1,7 @@
 # remotes::install_github('ramikrispin/coronavirus')
 # library(coronavirus)
 # update_datasets(silence=TRUE)
-pacman::p_load(char=c('tidyverse','janitor', 'ggrepel'))
+pacman::p_load(char=c('tidyverse','janitor', 'ggrepel', 'gghighlight'))
 
 # Due to changes in JHU data storage, I'm now directly downloading from JHU repo
 # rather than use the coronavirus package.
@@ -14,42 +14,42 @@ deaths <- read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/ma
   ungroup() %>%
   arrange(date)
 
-
-top_ten <- deaths %>%
-  filter(date==max(date)) %>%
-  top_n(11, deaths)
-
-deaths_top10 <-
-  deaths %>%
-  filter(country_region %in% top_ten$country_region) %>%
-  group_by(country_region) %>%
+deaths_after_10 <- deaths %>%
   filter(deaths >= 10) %>%
+  arrange(date) %>%
+  group_by(country_region) %>%
   mutate(days = as.numeric(date - min(date))) %>%
   ungroup()
 
-(staticplot <- ggplot(deaths_top10, aes(x = days, y = deaths)) +
-  geom_label_repel(data = deaths_top10 %>%
-                     filter(country_region %in% top_ten$country_region) %>%
-                     group_by(country_region) %>%
-                     filter(days == max(days)) %>%
-                     ungroup(),
-                   aes(color = country_region, label = country_region),
-                  vjust=1, hjust = 1, show.legend=F)+
+top_ten <- deaths_after_10 %>%
+  filter(date==max(date)) %>%
+  top_n(10, deaths)
+
+
+staticplot <- ggplot(deaths_after_10, aes(x = days, y = deaths)) +
   geom_line(aes(color = country_region), show.legend = F)+
-  geom_point(data = deaths_top10 %>%
-               filter(country_region %in% top_ten$country_region) %>%
+  geom_point(data = deaths_after_10 %>%
                group_by(country_region) %>%
                filter(days == max(days)) %>%
                ungroup(),
              aes(color = country_region, size=deaths),
               show.legend=F)+
-    geom_abline(intercept=1, slope = log10(1.33), linetype=2)+
-    annotate('text',label = '33% growth', x = 21, y = 10^(1+log10(1.33)*21),
-             angle = 63, vjust=0)+
+  gghighlight(country_region %in% top_ten$country_region)+
+  geom_abline(intercept = log10(10), slope = log10(2)/c(2,4,6),
+              color = 'grey', linetype=2)+
   scale_y_log10('Cumulative deaths')+
   labs(x = 'Days since 10th death')+
   labs(caption = glue::glue('Source: JHU, updated {max(deaths$date)}'))+
   theme_classic()
-)
+
+
+x_range <- ggplot_build(staticplot)$layout$panel_scales_x[[1]]$range$range
+y_range <- ggplot_build(staticplot)$layout$panel_scales_y[[1]]$range$range
+y_target <- y_range[1] + diff(y_range)*0.95
+x_target <- (y_target - y_range[1])/(log10(2)/c(2,4,6))
+x_0 <- x_range[1] + diff(x_range)*0.05
+
+staticplot + annotate('text', y = 10^y_target, x = x_0, label = 'Doubling in ...')+
+  annotate('text', y = 10^y_target, x = x_target, label = paste(c(2,4,6), 'days'))
 
 # interactive_plot <- plotly::ggplotly(staticplot)
